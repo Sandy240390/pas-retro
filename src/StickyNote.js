2a664e806e6938f2db5e651f8e4f6076583bc892
Sandeep <sandeep.gadappa@gmail.com>
2020-07-07 20:41:18 +0530

    add now

---
+++ src/StickyNote.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class StickyNote extends Component {
+
+    render() {
+
+        return(
+            <>
+                <div></div>
+                <textarea></textarea>
+            </>
+        );
+    }
+}
+
+export default StickyNote;
